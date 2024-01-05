0b3a194ac9c9da3efd2d932f0660b6f0c2d2956d
Sarwar Jahan <shohidmax@gmail.com>
2024-01-06 04:23:15 +0600

    new env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3005;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.Db_user}:${process.env.db_pass}@ums.b2w96to.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://$umsdubai:1Tieyhtu1IRJR0rI@ums.b2w96to.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
